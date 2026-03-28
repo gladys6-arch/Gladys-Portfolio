bf9a272688a4e207d10f121744155509ee1acce2
gladys6-arch <gladys6-arch@github.com>
2026-03-29 00:14:35 +0300

    add skills and contact files

---
+++ index.js
@@ -0,0 +1,8 @@
+const Projects = [
+  {
+    title: "Music App",
+    tech: "HTML, CSS, JavaScript",
+    description: "Responsive music player with play, pause and skip controls,",
+    github: "https://github.com/gladys6-arch/music-app"
+  },
+]
